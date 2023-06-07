ae40b2820748b64189dda501bd749ddf21c5e3c4
beatkapo <kapobeatbox22@gmail.com>
2023-06-07 04:40:26 +0200

    Funciona perfecto en el servidor indicado

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const client = new Client({
   partials: [Object.keys(Partials)]
 });
 const fs = require('fs');
-const botOwnerId = 'TU_ID_DE_USUARIO'; // Reemplaza con tu ID de usuario
-const requiredRole = 'KAPO'; // Reemplaza con el nombre del rol requerido
-const workerRanks = ['uno', 'dos', 'tres']; // Reemplaza con los rangos de trabajador en orden ascendente
+const botOwnerId = 'beatkapo'; // Reemplaza con tu ID de usuario
+const requiredRole = 'Propietario'; // Reemplaza con el nombre del rol requerido
+const workerRanks = ['Ayudante', 'Mozo', 'Ranchero']; // Reemplaza con los rangos de trabajador en orden ascendente
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+  console.log(`Conectado como ${client.user.tag}`);
 });
 
 client.on('messageCreate', async (message) => {
+  
   if (message.content.startsWith('/contratar')) {
     // Verificar si el autor del mensaje es el propietario del bot o tiene el rol requerido
     if (!message.member.roles.cache.some((role) => role.name === requiredRole)) {
@@ -25,6 +26,13 @@ client.on('messageCreate', async (message) => {
     if (!userToContract) {
       return message.reply('Debes mencionar a un usuario válido.');
     }
+    // const mentionedRoles = userToContract.roles.cache.map(role => role.name);
+
+    // const hasAllowedRole = workerRanks.some(role => mentionedRoles.includes(role));
+
+    // if (hasAllowedRole) {
+    //   message.reply(`El usuario ya forma parte de la plantilla.`);
+    // } 
 
     try {
       // Obtener el rol que deseas asignar al usuario
@@ -42,7 +50,7 @@ client.on('messageCreate', async (message) => {
 
 
       // Responder en el canal de texto indicando que se ha contratado al usuario
-      message.channel.send(`Se ha contratado a ${userToContract.username}.`);
+      message.channel.send(`Se ha contratado a ${message.guild.members.cache.get(userToContract.id).displayName}.`);
     } catch (error) {
       console.error('Error al contratar al usuario:', error);
       message.reply('Ocurrió un error al intentar contratar al usuario.');
@@ -50,32 +58,44 @@ client.on('messageCreate', async (message) => {
   }
 
   if (message.content.startsWith('/ascender')) {
-    // Obtener el usuario mencionado en el comando
-    const mentionedUser = message.mentions.users.first();
-    if (!mentionedUser) {
-      return message.reply('Debes mencionar a un usuario válido para ascender.').then(reply => {
-        reply.delete({ timeout: 5000 });
-      });
+    // Verificar si el autor del mensaje es el propietario del bot o tiene el rol requerido
+    if (message.author.id !== botOwnerId && !message.member.roles.cache.some((role) => role.name === requiredRole)) {
+      return message.reply('No tienes los permisos necesarios para usar este comando.');
+    }
+
+    // Obtener la mención del usuario a ascender
+    const userToPromote = message.mentions.users.first();
+    if (!userToPromote) {
+      return message.reply('Debes mencionar a un usuario válido para ascenderlo.');
     }
 
     try {
-      // Obtener el rango actual del trabajador
-      const currentRank = workersRanks.find(rank => message.member.roles.cache.some(role => role.name === rank));
+      // Obtener el rol actual del usuario
+      const userMember = message.guild.members.cache.get(userToPromote.id);
+      const currentRoles = userMember.roles.cache;
+      const currentRank = workerRanks.find(rank => currentRoles.some(role => role.name === rank));
+
       if (!currentRank) {
-        return message.reply('No tienes un rango válido para ascender.').then(reply => {
-          reply.delete({ timeout: 5000 });
-        });
+        return message.reply('El usuario no tiene un rango de trabajador válido.');
       }
 
-      // Obtener el índice del rango actual en la lista de rangos
-      const currentRankIndex = workersRanks.indexOf(currentRank);
+      // Obtener el siguiente rango de trabajador
+      const nextRankIndex = workerRanks.indexOf(currentRank) + 1;
+      if (nextRankIndex >= workerRanks.length) {
+        return message.reply('No se puede ascender más al usuario, ya tiene el rango máximo."');
+      }
+      const nextRank = workerRanks[nextRankIndex];
 
-      // Obtener el siguiente rango disponible
-      const nextRank = workerRanks[currentRankIndex + 1];
-      if (!nextRank) {
-        return message.reply('Ya tienes el rango más alto disponible.')
+      // Obtener el rol correspondiente al siguiente rango
+      const nextRankRole = message.guild.roles.cache.find(role => role.name === nextRank);
+      if (!nextRankRole) {
+        return message.reply('No se pudo encontrar el rol correspondiente al siguiente rango.');
+
       }
 
+      // Remover el rol actual y asignar el rol del siguiente rango al usuario
+      await userMember.roles.remove(currentRoles);
+      await userMember.roles.add(nextRankRole);
       const archivoTexto = `${nextRank.toLowerCase()}.txt`;
 
       // Leer el contenido del archivo de texto correspondiente
@@ -83,15 +103,14 @@ client.on('messageCreate', async (message) => {
 
       // Construir el mensaje para el usuario ascendido
       const mensajeAscenso = `¡Felicitaciones! Has sido ascendido a ${nextRank}.\n\n${contenidoTexto}`;
+      userMember.send(mensajeAscenso);
+      // Responder en el canal de texto indicando el ascenso del usuario
+      message.channel.send(`Se ha ascendido a ${message.guild.members.cache.get(userToPromote.id).displayName} al rango ${nextRank}.`);
 
-      // Enviar un mensaje privado al usuario ascendido con el mensaje completo
-      await mentionedUser.send(mensajeAscenso);
-
-      
     } catch (error) {
-        console.error('Error al ascender al usuario:', error);
-        message.reply('Ocurrió un error al intentar ascender al usuario.');
-      }
+      console.error('Error al ascender al usuario:', error);
+      message.reply('Ocurrió un error al intentar ascencder al usuario.');
+    }
   }
   if (message.content.startsWith('/descender')) {
     // Verificar si el autor del mensaje es el propietario del bot o tiene el rol requerido
@@ -126,15 +145,15 @@ client.on('messageCreate', async (message) => {
       const prevRankRole = message.guild.roles.cache.find(role => role.name === prevRank);
       if (!prevRankRole) {
         return message.reply('No se pudo encontrar el rol correspondiente al siguiente rango.');
-        
+
       }
 
       // Remover el rol actual y asignar el rol del siguiente rango al usuario
       await userMember.roles.remove(currentRoles);
-      await userMember.roles.add(prevRankRole); 
+      await userMember.roles.add(prevRankRole);
 
       // Responder en el canal de texto indicando el ascenso del usuario
-      message.channel.send(`Se ha descendido a ${userToPromote.username} al rango ${prevRank}.`);
+      message.channel.send(`Se ha descendido a ${message.guild.members.cache.get(userToPromote.id).displayName} al rango ${prevRank}.`);
     } catch (error) {
       console.error('Error al descender al usuario:', error);
       message.reply('Ocurrió un error al intentar descender al usuario.');
@@ -191,11 +210,16 @@ client.on('messageCreate', async (message) => {
       await mentionedUser.send(mensajeDespido);
 
       // Responder en el canal de texto indicando que se ha despedido al usuario
-      message.channel.send(`Se ha despedido a ${mentionedUser.username}. Razón: ${reason}`);
+      message.channel.send(`Se ha despedido a ${message.guild.members.cache.get(mentionedUser.id).displayName}. Razón: ${reason}`);
     } catch (error) {
       console.error('Error al despedir al usuario:', error);
       message.reply('Ocurrió un error al intentar despedir al usuario.');
     }
+  }
+  if (message.author.id !== client.user.id) {
+    if (message.author.bot) return; // Verifica si el autor del mensaje es un bot
+
+    message.delete();
   }
 });
 
